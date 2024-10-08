003e1cfb87ae2dc5dfc8be73fb9a2e6cb3260f19
kamekuramaisuke <taisukeoono350@gmail.com>
2024-10-08 18:18:32 +0900

    デザイン

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 	import { Badge } from '$lib/components/ui/badge';
 	import * as Menubar from '$lib/components/ui/menubar';
 	import { formatDate } from '../utils/date';
-	// import Mail from "lucide-svelte/icons/mail";
 	import Pencil from 'lucide-svelte/icons/pencil';
+	import { goto } from '$app/navigation';
 
 	let articles: Article[] = [];
 	onMount(async () => {
@@ -24,6 +24,15 @@
 	const deleteArticle = async (article: Article) => {
 		await remult.repo(Article).delete(article);
 		articles = articles.filter((a) => a.id !== article.id);
+	};
+
+	const onKeyDown = (e: KeyboardEvent) => {
+		switch (e.key) {
+			// 投稿画面
+			case 'n':
+				goto('/new');
+				break;
+		}
 	};
 </script>
 
@@ -42,7 +51,7 @@
 						<a href="/users">ユーザー管理</a>
 					</Menubar.Item>
 					<Menubar.Item>
-						<a href="/users">カテゴリ管理</a>
+						<a href="/categories">カテゴリ管理</a>
 					</Menubar.Item>
 				</Menubar.Content>
 			</Menubar.Menu>
@@ -58,16 +67,19 @@
 							<Table.Head>タイトル</Table.Head>
 							<Table.Head>作者</Table.Head>
 							<Table.Head>カテゴリ</Table.Head>
-							<Table.Head>作成日時</Table.Head>
+							<Table.Head>投稿日時</Table.Head>
 						</Table.Row>
 					</Table.Header>
 					<Table.Body>
 						{#each articles as article (article.id)}
 							<Table.Row>
-								<Table.Cell><a href="articles/{article.id}">{article.title}</a></Table.Cell>
+								<Table.Cell>{article.title}</Table.Cell>
 								<Table.Cell>{article.user?.name}</Table.Cell>
 								<Table.Cell><Badge variant="secondary">{article.category?.name}</Badge></Table.Cell>
 								<Table.Cell>{formatDate(article.createdAt)}</Table.Cell>
+								<Table.Cell>
+									<Button href="/articles/{article.id}" variant="outline">詳細</Button>
+								</Table.Cell>
 								<Table.Cell>
 									<Button on:click={() => deleteArticle(article)} variant="outline">削除</Button>
 								</Table.Cell>
@@ -79,6 +91,8 @@
 		</Card.Root>
 	</main>
 </div>
+
+<svelte:window on:keydown={onKeyDown} />
 
 <style>
 </style>
--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -3,6 +3,9 @@
 	import { remult } from 'remult';
 	import { onMount } from 'svelte';
 	import { Article } from '../../../shared/Article';
+	import * as Card from '$lib/components/ui/card';
+	import { Badge } from '$lib/components/ui/badge';
+	import { formatDate } from '../../../utils/date';
 
 	const articleId = Number($page.params.slug);
 	let article = {} as Article;
@@ -17,13 +20,25 @@
 	});
 </script>
 
-<main>
-	<article>
-		<h1>{article.title}</h1>
-		<div id="content">
-			{article.content}
-		</div>
-	</article>
+<main class="container mx-auto px-4 py-8">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title tag="h2">{article.title}</Card.Title>
+			<Badge class="w-1/12">{article.category?.name}</Badge>
+			<div>
+				<p class="text-sm font-medium">{article.user?.name}</p>
+				<div class="text-sm text-muted-foreground">
+					{formatDate(article.createdAt)}
+				</div>
+			</div>
+		</Card.Header>
+		<Card.Content>
+			<div id="content" class="max-w-none">
+				{article.content}
+			</div>
+		</Card.Content>
+	</Card.Root>
+	<a href="/">戻る</a>
 </main>
 
 <style>
